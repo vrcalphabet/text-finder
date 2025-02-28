4ba245d7382571e94df7630a49c9b8be3c3e9104
vrcalpha <wacca1024@gmail.com>
2025-02-28 12:20:26 +0900

    ビルドした拡張機能のcssインジェクション時のstyleのmimeタイプを変更

--- crx-patch/css-injection.js
+++ crx-patch/css-injection.js
@@ -5,6 +5,7 @@ async function p(path) {
   const url = chrome.runtime.getURL(path);
   const content = await fetch(url).then((res) => res.text());
   const style = document.createElement('style');
+  style.setAttribute('type', 'text/template');
   style.setAttribute('data-vite-dev-id', path);
   style.textContent = content;
   document.head.append(style);
