dab11021924bce04fdb762f51f7c7e7b0bbb8c5f
vrcalpha <wacca1024@gmail.com>
2025-03-01 14:29:51 +0900

    ビルド

--- dist/assets/main.tsx-loader-B-r4t4Kv.js
+++ dist/assets/main.tsx-loader-B-r4t4Kv.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/main.tsx-a3DzahFY.js")
+      chrome.runtime.getURL("assets/main.tsx-CUCzb0j-.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
