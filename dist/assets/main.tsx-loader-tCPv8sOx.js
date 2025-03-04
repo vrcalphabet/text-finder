890a62e1a856d603a816f626df33157e139fba1f
vrcalpha <wacca1024@gmail.com>
2025-03-04 23:01:35 +0900

    ビルド

--- dist/assets/main.tsx-loader-tCPv8sOx.js
+++ dist/assets/main.tsx-loader-tCPv8sOx.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/main.tsx-B_f3-Fui.js")
+      chrome.runtime.getURL("assets/main.tsx-CZRwnXvs.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
